573819378e18795300683604848bb112b3cdc686
Eric Seynaeve <github@nosperse.com>
2016-01-03 22:36:43 +0100

    converted to 4 spaces instead of 2 and added a CurrentSchedule object

--- src/SisPy/lib.py
+++ src/SisPy/lib.py
@@ -1,35 +1,87 @@
 #! /usr/bin/env python
 
+import struct
+
 
 class SisPy(object):
-  def __init__(self):
-    self._nr_outlets = 4
-    self._count_outlets_from_1 = True
-    self._time_in_GMT = True
+    def __init__(self):
+        self._nr_outlets = 4
+        self._count_outlets_from_1 = True
+        self._time_in_GMT = True
 
 
-  @property
-  def nr_outlets(self):
-    return self._nr_outlets
+    @property
+    def nr_outlets(self):
+        return self._nr_outlets
 
 
-  @property
-  def count_outlets_from_1(self):
-    return self._count_outlets_from_1
+    @property
+    def count_outlets_from_1(self):
+        return self._count_outlets_from_1
 
 
-  @count_outlets_from_1.setter
-  def count_outlets_from_1(self, b):
-    self._count_outlets_from_1 = b
+    @count_outlets_from_1.setter
+    def count_outlets_from_1(self, b):
+        self._count_outlets_from_1 = b
 
 
-  @property
-  def time_in_GMT(self):
-    return self._time_in_GMT
+    @property
+    def time_in_GMT(self):
+        return self._time_in_GMT
 
 
-  @time_in_GMT.setter
-  def time_in_GMT(self, b):
-    self._time_in_GMT = b
+    @time_in_GMT.setter
+    def time_in_GMT(self, b):
+        self._time_in_GMT = b
 
-# vim: set ai tabstop=2 shiftwidth=2 expandtab :
+class OutletCurrentSchedule(object):
+    def __init__(self, data, sispy):
+        self._data = data
+        self._sispy = sispy
+
+        self._timing_error = (data[0] & 0x80 == 0x80)
+        self._next_schedule_nr = (data[0] & 0x7f)
+        value = struct.unpack('<H', data[1:])[0]
+
+        if self._next_schedule_nr == 0x10:
+            # We're still waiting for the initial delay to finish
+            self._next_schedule_nr = 0
+            self._sequence_rampup = True
+            self._time_to_next_schedule = value
+        else:
+            self._next_schedule_nr = (data[0] & 0x7f)
+            self._sequence_rampup = False
+            self._time_to_next_schedule = (value & 0x3FFF)
+
+        self._switched_it_on = (value & 0x8000 == 0x8000)
+        self._sequence_done = (self._time_to_next_schedule == 0)
+
+    @property
+    def timing_error(self):
+        return self._timing_error
+
+    @property
+    def sequence_rampup(self):
+        return self._sequence_rampup
+
+    @property
+    def next_schedule_nr(self):
+        if self._sispy.count_outlets_from_1 == True:
+            return self._next_schedule_nr + 1
+        else:
+            return self._next_schedule_nr
+
+    @property
+    def switched_it_on(self):
+        return self._switched_it_on
+
+    @property
+    def time_to_next_schedule(self):
+        return self._time_to_next_schedule
+
+    @property
+    def sequence_done(self):
+        return self._sequence_done
+
+
+# vim: set ai tabstop=4 shiftwidth=4 expandtab :
--- tests/sispy_lib.py
+++ tests/sispy_lib.py
@@ -1,24 +1,89 @@
 #! /usr/bin/env python
 
 from SisPy.lib import SisPy
+from SisPy.lib import OutletCurrentSchedule
 
 import pytest
 
 
 @pytest.fixture
-def sispy(tmpdir):
-  return SisPy()
+def sispy():
+    return SisPy()
 
-def test_mock(tmpdir, sispy):
-  assert isinstance(sispy, SisPy)
 
-def test_property_defaults(tmpdir, sispy):
-  assert sispy.nr_outlets == 4
-  assert sispy.count_outlets_from_1 == True
-  sispy.count_outlets_from_1 = False
-  assert sispy.count_outlets_from_1 == False
-  assert sispy.time_in_GMT == True
-  sispy.time_in_GMT = False
-  assert sispy.time_in_GMT == False
+@pytest.fixture
+def outlet_current_schedule_data_ok_off(sispy):
+    return bytearray([0x01, 0x2, 0x0])
 
-# vim: set ai tabstop=2 shiftwidth=2 expandtab :
+
+@pytest.fixture
+def outlet_current_schedule_data_ok_on(sispy):
+    return bytearray([0x01, 0x2, 0x80])
+
+
+@pytest.fixture
+def outlet_current_schedule_data_error_off(sispy):
+    return bytearray([0x81, 0x2, 0x0])
+
+
+@pytest.fixture
+def outlet_current_schedule_data_ok_off_rampup(sispy):
+    return bytearray([0x10, 0x2, 0x0])
+
+
+@pytest.fixture
+def outlet_current_schedule_data_ok_off_done(sispy):
+    return bytearray([0x02, 0x0, 0x0])
+
+
+def test_mock(sispy):
+    assert isinstance(sispy, SisPy)
+
+
+def test_property_defaults(sispy):
+    assert sispy.nr_outlets == 4
+    assert sispy.count_outlets_from_1 == True
+    sispy.count_outlets_from_1 = False
+    assert sispy.count_outlets_from_1 == False
+    assert sispy.time_in_GMT == True
+    sispy.time_in_GMT = False
+    assert sispy.time_in_GMT == False
+
+
+def _test_outlet_current_schedule(current_schedule, sispy, timing_error = False, switched_it_on = False, time_to_next_schedule = 2,
+                                  next_schedule_nr = 2, sequence_rampup = False, sequence_done = False):
+    assert current_schedule.timing_error == timing_error
+    assert current_schedule.switched_it_on == switched_it_on
+    assert current_schedule.time_to_next_schedule == time_to_next_schedule
+    assert current_schedule.next_schedule_nr == next_schedule_nr
+    assert current_schedule.sequence_rampup == sequence_rampup
+    assert current_schedule.sequence_done == sequence_done
+
+    sispy.count_outlets_from_1 = False
+    assert current_schedule.next_schedule_nr == next_schedule_nr - 1
+    
+def test_outlet_current_schedule_ok_off(sispy, outlet_current_schedule_data_ok_off):
+    current_schedule = OutletCurrentSchedule(outlet_current_schedule_data_ok_off, sispy)
+    _test_outlet_current_schedule(current_schedule, sispy)
+
+
+def test_outlet_current_schedule_error_off(sispy, outlet_current_schedule_data_error_off):
+    current_schedule = OutletCurrentSchedule(outlet_current_schedule_data_error_off, sispy)
+    _test_outlet_current_schedule(current_schedule, sispy, timing_error = True)
+
+
+def test_outlet_current_schedule_ok_on(sispy, outlet_current_schedule_data_ok_on):
+    current_schedule = OutletCurrentSchedule(outlet_current_schedule_data_ok_on, sispy)
+    _test_outlet_current_schedule(current_schedule, sispy, switched_it_on = True)
+
+
+def test_outlet_current_schedule_ok_off_rampup(sispy, outlet_current_schedule_data_ok_off_rampup):
+    current_schedule = OutletCurrentSchedule(outlet_current_schedule_data_ok_off_rampup, sispy)
+    _test_outlet_current_schedule(current_schedule, sispy, sequence_rampup = True, next_schedule_nr = 1)
+
+
+def test_outlet_current_schedule_ok_off_done(sispy, outlet_current_schedule_data_ok_off_done):
+    current_schedule = OutletCurrentSchedule(outlet_current_schedule_data_ok_off_done, sispy)
+    _test_outlet_current_schedule(current_schedule, sispy, sequence_done = True, time_to_next_schedule = 0, next_schedule_nr = 3)
+
+# vim: set ai tabstop=4 shiftwidth=4 expandtab :
