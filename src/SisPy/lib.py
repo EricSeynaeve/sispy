b61c78b727867068a116452042edd5e18d8ce281
Eric Seynaeve <github@nosperse.com>
2016-01-03 21:06:44 +0100

    making the relevant properties r/w

--- src/SisPy/lib.py
+++ src/SisPy/lib.py
@@ -1,21 +1,35 @@
 #! /usr/bin/env python
 
+
 class SisPy(object):
   def __init__(self):
     self._nr_outlets = 4
     self._count_outlets_from_1 = True
     self._time_in_GMT = True
 
+
   @property
   def nr_outlets(self):
     return self._nr_outlets
+
 
   @property
   def count_outlets_from_1(self):
     return self._count_outlets_from_1
 
+
+  @count_outlets_from_1.setter
+  def count_outlets_from_1(self, b):
+    self._count_outlets_from_1 = b
+
+
   @property
   def time_in_GMT(self):
     return self._time_in_GMT
+
+
+  @time_in_GMT.setter
+  def time_in_GMT(self, b):
+    self._time_in_GMT = b
 
 # vim: set ai tabstop=2 shiftwidth=2 expandtab :
--- tests/sispy_lib.py
+++ tests/sispy_lib.py
@@ -15,6 +15,10 @@ def test_mock(tmpdir, sispy):
 def test_property_defaults(tmpdir, sispy):
   assert sispy.nr_outlets == 4
   assert sispy.count_outlets_from_1 == True
+  sispy.count_outlets_from_1 = False
+  assert sispy.count_outlets_from_1 == False
   assert sispy.time_in_GMT == True
+  sispy.time_in_GMT = False
+  assert sispy.time_in_GMT == False
 
 # vim: set ai tabstop=2 shiftwidth=2 expandtab :
