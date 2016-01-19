6ddd80c68ce816807ff556438d2e1cdcee1fcd87
Eric Seynaeve <github@nosperse.com>
2016-01-19 09:09:28 +0100

    Adding examples

---
+++ examples/readme_example.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+import time
+
+from SisPy.lib import SisPy
+
+sispy = SisPy()
+
+my_outlet = sispy.outlets[0]
+# make sure the outlet is switched on
+my_outlet.switched_on = True
+
+my_schedule = my_outlet.schedule
+
+# start a period schedule from scratch
+my_schedule.reset()
+my_schedule.periodic = True
+my_schedule.add_entry()
+# adjust this time to your own liking
+my_schedule.entries[0].start_time = time.strptime('2016-01-19 20:30:00 UTC', '%Y-%m-%d %H:%M:%S %Z')
+# switch the outlet off for 14 hours
+my_schedule.entries[0].switch_on = False
+my_schedule.entries[0].minutes_to_next_schedule_entry = 14 * 60
+my_schedule.add_entry()
+# then switch it on for 10 hours
+my_schedule.entries[1].switch_on = True
+my_schedule.entries[1].minutes_to_next_schedule_entry = 10 * 60
+
+# make sure the periodicity is 24 hours
+assert my_schedule.periodicity_minutes == 24 * 60
+
+# store the schedule on the power strip
+my_schedule.apply()
---
+++ examples/readout.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+import time
+import calendar
+
+
+from SisPy.lib import SisPy
+from SisPy.lib import _min2human as m2h
+
+sispy = SisPy()
+
+print "ID: " + str(sispy.id)
+for i in range(4):
+    outlet = sispy.outlets[i]
+    print "Outlet", i, ":"
+    print "  Switched on ? " + str(outlet.switched_on)
+    current_entry = outlet.current_schedule_entry
+    print "  # schedule entries: " + str(len(outlet.schedule.entries))
+    print "  Schedule set at:    " + time.strftime("%Y-%m-%d %H:%M:%S UTC", outlet.schedule.time_activated)
+    if len(outlet.schedule.entries) > 0:
+        print "  Periodic schedule ? " + str(outlet.schedule.periodic)
+        print "  Current schedule entry:"
+        schedule_nr = current_entry.current_schedule_nr
+        if schedule_nr is not None:
+            print "      Switched on at start ?  " + str(current_entry.switched_it_on)
+            print "      # of current entry:     " + str(current_entry.current_schedule_nr)
+            print "      Start of current entry: " + time.strftime("%Y-%m-%d %H:%M:%S UTC", outlet.schedule.entries[current_entry.current_schedule_nr].start_time)
+        print "      Minutes to next entry:  " + str(current_entry.minutes_to_next_schedule_entry)
+        print "      Start of next entry:    " + time.strftime("%Y-%m-%d %H:%M UTC +/- 1 min", time.gmtime(time.time() + current_entry.minutes_to_next_schedule_entry * 60))
+    for e in range(len(outlet.schedule.entries)):
+        schedule_entry = outlet.schedule.entries[e]
+        print "  Entry #%-2i:" % (e,),
+        print "Switch on at start ? %-5s |" % (str(schedule_entry.switch_on),),
+        print "Start time: " + time.strftime("%Y-%m-%d %H:%M:%S UTC", schedule_entry.start_time),"|",
+        print "Minutes active: " + m2h(schedule_entry.minutes_to_next_schedule_entry)
+    print
+
+# vim: set ai tabstop=4 shiftwidth=4 expandtab :
